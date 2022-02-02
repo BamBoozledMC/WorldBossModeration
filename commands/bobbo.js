d09c0f09a290e77b55b1a94f34f146f822dabe40
Bamboozled <64771089+BamBoozledMC@users.noreply.github.com>
2022-02-02 20:41:16 +1000

    update bobbo command

--- commands/bobbo.js
+++ commands/bobbo.js
@@ -1,5 +1,5 @@
 const config = require('../config.json');
-const Discord = require ("discord.js");
+const {Discord, MessageAttachment} = require ("discord.js");
 
 module.exports = {
 	name: 'bobbo',
@@ -7,10 +7,11 @@ module.exports = {
 	usage: '<message>',
 	args: true,
 	async execute(bot, message, args, prefix) {
-		if (!message.member.hasPermission("MANAGE_MESSAGES") && message.author.id != config.ownerID) return;
-		if (message.author.bot) return;
-
-		message.channel.send("https://images-ext-2.discordapp.net/external/JYYplPhFXMOIbvNeo_aHiyYPVJAW6uccwAZkhTmJrwI/%3Fssl%3D1/https/i0.wp.com/www.sandwichtribunal.com/wp-content/uploads/2020/02/Men_At_Work_-_Down_Under_Official_Video.gif").catch(error =>{
+		if(!message.member.hasPermission("MANAGE_MESSAGES") && message.author.id != config.ownerID) {
+			if(!message.member.roles.cache.some(role => role.id === '937561741334814810')) return;
+		}
+		const picture = new MessageAttachment('./millsy.png');
+		message.channel.send(picture).catch(error =>{
 		})
     }
 }
