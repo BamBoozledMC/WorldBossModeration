69b8047b516e3459a27c293d351803be2004cc20
Bamboozled <64771089+BamBoozledMC@users.noreply.github.com>
2022-02-15 15:59:08 +1000

    Create woody.js

---
+++ commands/woody.js
@@ -0,0 +1,16 @@
+const config = require('../config.json');
+const {Discord, MessageAttachment} = require ("discord.js");
+
+module.exports = {
+	name: 'woody',
+  descrption: 'Returns your message',
+	usage: '<message>',
+	args: true,
+	async execute(bot, message, args, prefix) {
+		if(!message.member.hasPermission("MANAGE_MESSAGES") && message.author.id != config.ownerID) {
+			if(!message.member.roles.cache.some(role => role.id === '937561741334814810')) return;
+		}
+		message.channel.send("Woody is cool :sunglasses:").catch(error =>{
+		})
+    }
+}
