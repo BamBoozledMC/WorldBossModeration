fd87df5b5a1798e4a89f5d855e7cc44223bfc01b
Bamboozled <64771089+BamBoozledMC@users.noreply.github.com>
2022-03-03 15:55:23 +1000

    Revamp ping command

--- commands/ping.js
+++ commands/ping.js
@@ -5,12 +5,10 @@ const ping = require('ping');
 module.exports = {
 	name: 'ping',
 	description: 'Checks the bot\'s ping',
+	cooldown: 5,
 	usage: '!ping',
 	args: false,
 	async execute(bot, message, args, prefix) {
-		if(!message.member.hasPermission("MANAGE_MESSAGES") && message.author.id != config.ownerID) {
-			if(message.channel.id == config.generalID) return;
-		}
 		const pingMsg =  await message.channel.send('<a:loading:939665977728176168> Pinging...');
 		let localping = await ping.promise.probe('127.0.0.1', {
            timeout: 5,
@@ -19,17 +17,15 @@ module.exports = {
 	          timeout: 5,
 	      });
 		try {
-		return pingMsg.edit(`
-		Pong! __Roundtrip__: **${
-				(pingMsg.editedTimestamp || pingMsg.createdTimestamp) - (message.editedTimestamp || message.createdTimestamp)
-			}ms.** | __API__: **${
-				bot.ws.ping
-			}ms** | __ISP/DNS__: **${
-				isp_dns.time
-			}ms** | __INTERNAL__: **${
-				localping.time
-			}ms**
-		`);
+			let pingembed = new Discord.MessageEmbed()
+			.setTitle("Pong! 🏓")
+			.addField("📥 Roundtrip:", `**${(pingMsg.editedTimestamp || pingMsg.createdTimestamp) - (message.editedTimestamp || message.createdTimestamp)}ms**`)
+			.addField("📤 API:", `**${bot.ws.ping}ms**`)
+			.addField("<:server:948743195309768746> ISP/DNS:", `**${isp_dns.time}ms**`)
+			.addField("🖥️ INTERNAL:", `**${localping.time}ms**`)
+			.setColor("#d90053")
+			.setTimestamp()
+		return pingMsg.edit("", pingembed);
 	}catch(e){}
 }
 }
