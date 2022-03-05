1fda4051585ea9a28de857298a2772d8b648d3fc
Bamboozled <64771089+BamBoozledMC@users.noreply.github.com>
2022-03-06 00:29:54 +1000

    fix error

--- commands/lurklist.js
+++ commands/lurklist.js
@@ -23,9 +23,11 @@ module.exports = {
 		let getreason = declarearray.reason
 		let getID = declarearray.userID
 		let getTag = bot.users.cache.get(getID);
-		lurklist.addField(`${getTag.tag}`, `Reason: ${getreason}`).catch(error =>{
+		try {
+		lurklist.addField(`${getTag.tag}`, `Reason: ${getreason}`)
+	} catch(e){
 			return message.channel.send("error, check console for info")
-		})
+		}
     numofusers++
 });
 lurklist.setDescription(`There are **${numofusers}** lurkers`)
