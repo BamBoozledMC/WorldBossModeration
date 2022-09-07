a79f6cdf4f8d686ee7938b3a2890baa966281607
Bamboozled <64771089+BamBoozledMC@users.noreply.github.com>
2022-09-07 18:58:51 +1000

    fixed tictactoe slash command not giving stats

--- slashcommands.js
+++ slashcommands.js
@@ -452,3 +452,38 @@ module.exports = async (interaction, bot) => {
   }
   }
 }
+
+game.on('win', data => {
+	let winner;
+	if(data.winner.id == 'AI' ){
+		winner = data.winner.id
+	} else {
+		winner = data.winner.user.id
+	}
+
+	let loser;
+	if(data.loser.id == 'AI' ){
+		loser = data.loser.id
+	} else {
+		loser = data.loser.user.id
+	}
+	db.add(`games.tictactoe.${winner}.wins`, 1)
+	db.add(`games.tictactoe.${loser}.losses`, 1)
+});
+game.on('tie', data => {
+	let p1;
+	if(data.players[0].id == 'AI' ){
+		p1 = data.players[0].id
+	} else {
+		p1 = data.players[0].user.id
+	}
+
+	let p2;
+	if(data.players[1].id == 'AI' ){
+		p2 = data.players[1].id
+	} else {
+		p2 = data.players[1].user.id
+	}
+	db.add(`games.tictactoe.${p1}.ties`, 1)
+	db.add(`games.tictactoe.${p2}.ties`, 1)
+});
